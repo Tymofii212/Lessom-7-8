f0e1d66482e4283d975d07681b4fc6b33940f849
Tymofii212 <tymofiimoysey@gmail.com>
2024-11-02 09:11:14 -0500

    Counter part

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
-from copyreg import constructor
+# from copyreg import constructor
+#
+#
+# class Worker:
+#     def __init__(self, name, age):
+#         self.Name=name
+#         self.Age=age
+#
+#
+# worker_list=[Worker("Olexander",22),
+#              Worker("Maxim",14),
+#              Worker("Oleh", 54)]
+#
+#
+# print(worker_list)
+#
+# worker_iter=iter(worker_list)
+#
+# for elem in worker_iter:
+#     print(elem.Name, elem.Age)
 
 
-class Worker:
-    def __init__(self, name, age):
-        self.Name=name
-        self.Age=age
+class Counter:
+    def __init__(self, max_value):
+        self.value = 0
+        self.max_value=max_value
 
+    def __iter__(self):
+        self.value=0
+        return self
 
-worker_list=[Worker("Olexander",22),
-             Worker("Maxim",14),
-             Worker("Oleh", 54)]
+    def __next__(self):
+        self.value += 1
+        if self.value > self.max_value:
+            raise StopIteration
+        return self.value
 
+timer = Counter(10)
 
-print(worker_list)
-
-worker_iter=iter(worker_list)
-
-for elem in worker_iter:
-    print(elem.Name, elem.Age)
-
-
-
+print(next(timer))
+print(next(timer))
+print(next(timer))
+print(next(timer))
+print(next(timer))
+print(next(timer))
+print(next(timer))
